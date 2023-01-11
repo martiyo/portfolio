47e2961325656ad6a1ef7ef5f487e2ef3ce879ea
dev1 <martiyosrc@gmail.com>
2023-01-10 21:35:33 -0300

    boton descargar curriculum

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function seleccionar() {
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
+
+function download(pdfUrl) {
+  fetch(pdfUrl)
+    .then((resp) => resp.arrayBuffer())
+    .then((resp) => {
+      const file = new Blob([resp], { type: "application/pdf" });
+
+      const fileURL = URL.createObjectURL(file);
+      const link = document.createElement("a");
+      link.href = fileURL;
+      link.download = "curriculum " + new Date() + ".pdf";
+      link.click();
+    });
+}
